0f53e45c329e89c3d7a707fc80de88771052f059
Lee Fine <50836957+leefine02@users.noreply.github.com>
2022-10-12 15:54:25 -0400

    Initial version (#1) (#2)

---
+++ RemoteFile/Discovery.cs
@@ -0,0 +1,89 @@
+﻿// Copyright 2021 Keyfactor
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Extensions;
+using Keyfactor.Orchestrators.Common.Enums;
+
+using Microsoft.Extensions.Logging;
+
+namespace Keyfactor.Extensions.Orchestrator.RemoteFile
+{
+    public class Discovery: IDiscoveryJobExtension
+    {
+        public string ExtensionName => "";
+
+        public JobResult ProcessJob(DiscoveryJobConfiguration config, SubmitDiscoveryUpdate submitDiscovery)
+        {
+            ILogger logger = LogHandler.GetClassLogger(this.GetType());
+            logger.LogDebug($"Begin {config.Capability} for job id {config.JobId}...");
+            logger.LogDebug($"Server: { config.ClientMachine }");
+            logger.LogDebug($"Job Properties:");
+            foreach (KeyValuePair<string, object> keyValue in config.JobProperties)
+            {
+                logger.LogDebug($"    {keyValue.Key}: {keyValue.Value}");
+            }
+
+            string[] directoriesToSearch = config.JobProperties["dirs"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] extensionsToSearch = config.JobProperties["extensions"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] ignoredDirs = config.JobProperties["ignoreddirs"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] filesTosearch = config.JobProperties["patterns"].ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            bool includeSymLinks = Convert.ToBoolean(config.JobProperties["symLinks"]);
+
+            List<string> locations = new List<string>();
+
+            RemoteCertificateStore certificateStore = new RemoteCertificateStore(config.ClientMachine, config.ServerUsername, config.ServerPassword, directoriesToSearch[0].Substring(0, 1) == "/" ? RemoteCertificateStore.ServerTypeEnum.Linux : RemoteCertificateStore.ServerTypeEnum.Windows);
+
+            try
+            {
+                ApplicationSettings.Initialize(this.GetType().Assembly.Location);
+
+                if (directoriesToSearch.Length == 0)
+                    throw new RemoteFileException("Blank or missing search directories for Discovery.");
+                if (extensionsToSearch.Length == 0)
+                    throw new RemoteFileException("Blank or missing search extensions for Discovery.");
+                if (filesTosearch.Length == 0)
+                    filesTosearch = new string[] { "*" };
+
+                locations = certificateStore.FindStores(directoriesToSearch, extensionsToSearch, filesTosearch, includeSymLinks);
+                foreach (string ignoredDir in ignoredDirs)
+                    locations = locations.Where(p => !p.StartsWith(ignoredDir)).ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Exception for {config.Capability}: {RemoteFileException.FlattenExceptionMessages(ex, string.Empty)} for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = RemoteFileException.FlattenExceptionMessages(ex, $"Server {config.ClientMachine}:") };
+            }
+            finally
+            {
+                certificateStore.Terminate();
+            }
+
+            try
+            {
+                logger.LogDebug($"Stores returned for {config.Capability}:");
+                foreach (string location in locations)
+                {
+                    logger.LogDebug($"    {location}");
+                }
+                submitDiscovery.Invoke(locations);
+                logger.LogDebug($"...End {config.Capability} job for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = RemoteFileException.FlattenExceptionMessages(ex, string.Empty);
+                logger.LogError($"Exception returning store locations for {config.Capability}: {errorMessage} for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = $"Server {config.ClientMachine}: {errorMessage}" };
+            }
+        }
+    }
+}
---
+++ RemoteFile/InventoryBase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+using Keyfactor.Orchestrators.Extensions;
+using Keyfactor.Orchestrators.Common.Enums;
+using Keyfactor.Logging;
+using Keyfactor.Extensions.Orchestrator.RemoteFile.Models;
+
+using Microsoft.Extensions.Logging;
+
+namespace Keyfactor.Extensions.Orchestrator.RemoteFile
+{
+    public abstract class InventoryBase : RemoteFileJobTypeBase, IInventoryJobExtension
+    {
+        protected ILogger logger;
+
+        public string ExtensionName => string.Empty;
+
+        RemoteCertificateStore certificateStore = new RemoteCertificateStore();
+
+        public JobResult ProcessJob(InventoryJobConfiguration config, SubmitInventoryUpdate submitInventory)
+        {
+            ILogger logger = LogHandler.GetClassLogger(this.GetType());
+            logger.LogDebug($"Begin {config.Capability} for job id {config.JobId}...");
+            logger.LogDebug($"Server: { config.CertificateStoreDetails.ClientMachine }");
+            logger.LogDebug($"Store Path: { config.CertificateStoreDetails.StorePath }");
+            logger.LogDebug($"Job Properties:");
+            foreach (KeyValuePair<string, object> keyValue in config.JobProperties ?? new Dictionary<string,object>())
+            {
+                logger.LogDebug($"    {keyValue.Key}: {keyValue.Value}");
+            }
+
+            ICertificateStoreSerializer certificateStoreSerializer = GetCertificateStoreSerializer();
+            List<CurrentInventoryItem> inventoryItems = new List<CurrentInventoryItem>();
+
+            try
+            {
+                ApplicationSettings.Initialize(this.GetType().Assembly.Location);
+                certificateStore = new RemoteCertificateStore(config.CertificateStoreDetails.ClientMachine, config.ServerUsername, config.ServerPassword, config.CertificateStoreDetails.StorePath, config.CertificateStoreDetails.StorePassword, config.JobProperties);
+                certificateStore.LoadCertificateStore(certificateStoreSerializer, config.CertificateStoreDetails.Properties);
+
+                List<X509Certificate2Collection> collections = certificateStore.GetCertificateChains();
+
+                logger.LogDebug($"Format returned certificates BEGIN");
+                foreach (X509Certificate2Collection collection in collections)
+                {
+                    if (collection.Count == 0)
+                        continue;
+
+                    X509Certificate2Ext issuedCertificate = (X509Certificate2Ext)collection[0];
+
+                    List<string> certChain = new List<string>();
+                    foreach (X509Certificate2 certificate in collection)
+                    {
+                        certChain.Add(Convert.ToBase64String(certificate.Export(X509ContentType.Cert)));
+                        logger.LogDebug(Convert.ToBase64String(certificate.Export(X509ContentType.Cert)));
+                    }
+
+                    inventoryItems.Add(new CurrentInventoryItem()
+                    {
+                        ItemStatus = OrchestratorInventoryItemStatus.Unknown,
+                        Alias = string.IsNullOrEmpty(issuedCertificate.FriendlyNameExt) ? issuedCertificate.Thumbprint : issuedCertificate.FriendlyNameExt,
+                        PrivateKeyEntry = issuedCertificate.HasPrivateKey,
+                        UseChainLevel = collection.Count > 1,
+                        Certificates = certChain.ToArray()
+                    });
+                }
+                logger.LogDebug($"Format returned certificates END");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Exception for {config.Capability}: {RemoteFileException.FlattenExceptionMessages(ex, string.Empty)} for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = RemoteFileException.FlattenExceptionMessages(ex, $"Site {config.CertificateStoreDetails.StorePath} on server {config.CertificateStoreDetails.ClientMachine}:") };
+            }
+            finally
+            {
+                if (certificateStore.RemoteHandler != null)
+                    certificateStore.Terminate();
+            }
+
+            try
+            {
+                submitInventory.Invoke(inventoryItems);
+                logger.LogDebug($"...End {config.Capability} job for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = RemoteFileException.FlattenExceptionMessages(ex, string.Empty);
+                logger.LogError($"Exception returning certificates for {config.Capability}: {errorMessage} for job id {config.JobId}");
+                return new JobResult() { Result = OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = RemoteFileException.FlattenExceptionMessages(ex, $"Site {config.CertificateStoreDetails.StorePath} on server {config.CertificateStoreDetails.ClientMachine}:") };
+            }
+        }
+    }
+}
