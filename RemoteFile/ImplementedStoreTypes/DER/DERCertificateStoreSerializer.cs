f7c49281a286b877f85adccb70f9205dec4978ee
kfadmin <kfadmin@LFINE-KF10-ORCH>
2023-01-24 19:54:07 +0000

    changes

--- RemoteFile/ImplementedStoreTypes/DER/DERCertificateStoreSerializer.cs
+++ RemoteFile/ImplementedStoreTypes/DER/DERCertificateStoreSerializer.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿// Copyright 2021 Keyfactor
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
+using System.Linq;
 
 using Newtonsoft.Json;
 
 using Keyfactor.Logging;
 using Keyfactor.PKI.PrivateKeys;
 using Keyfactor.PKI.X509;
-using Keyfactor.PKI.PEM;
 using Keyfactor.Extensions.Orchestrator.RemoteFile.RemoteHandlers;
 using Keyfactor.Extensions.Orchestrator.RemoteFile.Models;
 
@@ -17,9 +23,6 @@ using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.X509;
-using Org.BouncyCastle.Tls;
-using Org.BouncyCastle.Ocsp;
-using System.Linq;
 
 namespace Keyfactor.Extensions.Orchestrator.RemoteFile.DER
 {
