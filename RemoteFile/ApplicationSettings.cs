3070f011e37ce56b3d615499c66dd852f9a5526f
Lee Fine <50836957+leefine02@users.noreply.github.com>
2024-03-15 14:30:18 +0000

    Ab#55018 (#49)

--- RemoteFile/ApplicationSettings.cs
+++ RemoteFile/ApplicationSettings.cs
@@ -27,6 +27,7 @@ namespace Keyfactor.Extensions.Orchestrator.RemoteFile
 
         private const string DEFAULT_LINUX_PERMISSION_SETTING = "600";
         private const string DEFAULT_OWNER_SETTING = "";
+        private const string DEFAULT_SUDO_IMPERSONATION_SETTING = "";
 
         private static Dictionary<string,string> configuration;
 
@@ -36,6 +37,7 @@ namespace Keyfactor.Extensions.Orchestrator.RemoteFile
         public static string SeparateUploadFilePath { get { return configuration.ContainsKey("SeparateUploadFilePath") ? AddTrailingSlash(configuration["SeparateUploadFilePath"]) : string.Empty; } }
         public static string DefaultLinuxPermissionsOnStoreCreation { get { return configuration.ContainsKey("DefaultLinuxPermissionsOnStoreCreation") ? configuration["DefaultLinuxPermissionsOnStoreCreation"] : DEFAULT_LINUX_PERMISSION_SETTING; } }
         public static string DefaultOwnerOnStoreCreation { get { return configuration.ContainsKey("DefaultOwnerOnStoreCreation") ? configuration["DefaultOwnerOnStoreCreation"] : DEFAULT_OWNER_SETTING; } }
+        public static string DefaultSudoImpersonatedUser { get { return configuration.ContainsKey("DefaultSudoImpersonatedUser") ? configuration["DefaultSudoImpersonatedUser"] : DEFAULT_SUDO_IMPERSONATION_SETTING; } }
         public static FileTransferProtocolEnum FileTransferProtocol 
         { 
             get 
