12c79f571ddabe7f66f7148548e814ee049732d5
Lee Fine <lfine@keyfactor.com>
2025-11-14 20:28:07 +0000

    initial-integration-tests

---
+++ RemoteFileIntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteFileIntegrationTests
+{
+    public abstract class BaseIntegrationTest : IDisposable
+    {
+        protected BaseIntegrationTest()
+        { 
+        
+        }
+
+        public void Dispose()
+        {
+            TearDown();
+        }
+
+        public abstract void TearDown();
+    }
+}
---
+++ RemoteFileIntegrationTests/RFPEMIntegrationTests.cs
@@ -0,0 +1,15 @@
+namespace RemoteFileIntegrationTests
+{
+    public class RFPEMIntegrationTests : BaseIntegrationTest
+    {
+        [Fact(DisplayName = "RFPEM Inventory Internal Private Key")]
+        public void RFPEM_Inventory_InternalPrivateKey()
+        {
+
+        }
+        public override void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
